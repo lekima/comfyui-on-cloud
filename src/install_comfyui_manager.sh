45092e6d555a54730f98cbb2487882be3d0c0ab3
Minh Tran <git@minh.email>
2024-05-29 14:14:25 +0800

    Update install_comfyui_manager.sh

--- src/install_comfyui_manager.sh
+++ src/install_comfyui_manager.sh
@@ -2,7 +2,5 @@
 
 # make sure you are in comfyui installation folder
 
-#COMFY MANAGER
-
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git custom_nodes/ComfyUI-Manager
 pip install -r custom_nodes/ComfyUI-Manager/requirements.txt
